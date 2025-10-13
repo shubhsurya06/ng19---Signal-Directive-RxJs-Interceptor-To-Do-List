2c08121847bbd979c7ba021b22ec0539a1027e08
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-10-13 23:40:42 +0530

    added logs in todo.service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -4,16 +4,20 @@ import { Task } from '../task/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
-    private todoList: Task[] = [{
+    taskObj1: Task = {
         id: Date.now(),
         title: 'task 1',
         isCompleted: false
-    }];
+    }
 
+    private todoList: Task[] = [this.taskObj1];
+
+    // returns a todo list with a simulated delay
     getTodos(): Observable<Task[]> {
         return of([...this.todoList]).pipe(delay(300));
     }
 
+    // adds a new todo with a simulated delay
     addTodo(title: string): Observable<Task> {
         const newTodo: Task = {
             id: Date.now(),
@@ -24,6 +28,7 @@ export class TodoService {
         return of(newTodo).pipe(delay(300));
     }
 
+    // updates a todo title with a simulated delay
     updateTodo(id: number, title: string): Observable<Task> {
         const index = this.todoList.findIndex(t => t.id === id);
         if (index !== -1) {
@@ -35,11 +40,13 @@ export class TodoService {
         return of(undefined as unknown as Task).pipe(delay(300));
     }
 
+    // deletes a todo with a simulated delay
     deleteTodo(id: number): Observable<void> {
         this.todoList = this.todoList.filter(t => t.id !== id);
         return of(void 0).pipe(delay(300));
     }
 
+    // toggles a todo's completion status with a simulated delay
     toggleTodo(todo: Task): Observable<Task> {
         const index = this.todoList.findIndex(item => item.id === todo.id);
         if (index !== -1) {
