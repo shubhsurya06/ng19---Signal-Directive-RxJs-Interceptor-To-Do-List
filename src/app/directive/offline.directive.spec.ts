e871003c804d8cf370242ac7b8a17d2b3b834442
Shubham Suryawanshi <shubhsurya06@gmail.com>
2026-01-01 13:36:49 +0530

    offline directive added

---
+++ src/app/directive/offline.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OfflineDirective } from './offline.directive';
+
+describe('OfflineDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OfflineDirective();
+    expect(directive).toBeTruthy();
+  });
+});
